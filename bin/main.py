a7a9763d6fa69f99ea34def872fa6329840a88ce
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-06-23 21:19:22 +0200

    WIP : ajoute d'un systéme de variable + autoincrémentation

--- bin/main.py
+++ bin/main.py
@@ -8,7 +8,7 @@ init()
 class Compilateur:
     def __init__(self):
         # Liste des noms de fonctions autorisées
-        self.fonctions_autorisees = ["pyrint"]
+        self.fonctions_autorisees = ["pyrint", "+", "-", "*", "/", "%", "^=", "const"]
 
     def compiler(self, arguments):
         # Afficher le message de début de compilation
@@ -19,12 +19,22 @@ class Compilateur:
         for argument in arguments:
             with open(argument, "r") as file:
                 code = file.read()
+                matches = re.findall(r'const\s+(\w+)\s*=\s*(.+)', code)
+                variables = {}
+
+                for match in matches:
+                    variable_name, value = match
+                    value = self.evaluate_expression(value, variables)
+                    variables[variable_name] = value
+
                 matches = re.findall(r'([a-zA-Z_][a-zA-Z0-9_]*)\((.+?)\)', code)
                 for match in matches:
                     fonction, value = match
                     if fonction in self.fonctions_autorisees:
-                        value = value.strip('"')
-                        print(value)
+                        if fonction == "pyrint":
+                            value = value.strip('"')
+                            value = self.evaluate_expression(value, variables)
+                            print(value)
                     else:
                         suggestion = None
                         for f in self.fonctions_autorisees:
@@ -34,7 +44,7 @@ class Compilateur:
                         print(Fore.RED + "Erreur de frappe :", fonction)
                         if suggestion:
                             print("Vouliez-vous dire :", suggestion + "()" + Fore.RESET)
-                        compteur_erreurs += 1  # Incrémenter le compteur d'erreurs
+                        compteur_erreurs += 1
 
         # Afficher le message de fin de compilation et le nombre d'erreurs
         print(Fore.GREEN + "La compilation est terminée." + Fore.RESET)
@@ -42,6 +52,15 @@ class Compilateur:
             print(Fore.GREEN + "Aucune erreur de frappe détectée." + Fore.RESET)
         else:
             print(Fore.RED + "Nombre total d'erreurs de frappe :", str(compteur_erreurs) + Fore.RESET)
+
+    def evaluate_expression(self, expression, variables):
+        for variable_name, variable_value in variables.items():
+            expression = expression.replace(variable_name, variable_value)
+        try:
+            return str(eval(expression, {"const": lambda x: str(variables.get(x, x))}, {}))
+        except TypeError:
+            return expression
+
 
 # Utilisation de la classe Compilateur
 arguments = sys.argv[1:]  # Ignorer le premier argument (nom du script)
