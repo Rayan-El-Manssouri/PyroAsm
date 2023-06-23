74b90ccc0c57451cd6cc7f18704173efa1afbc15
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-06-23 02:50:54 +0200

    Feat : supression des fichier inutiles

--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set "ligne=pyrint("Premier programme de compilation et d'exécution de code PyAsm" "
-
-for /F "tokens=2 delims=()" %%a in ("%ligne%") do set "contenu=%%a"
-
-echo %contenu%
