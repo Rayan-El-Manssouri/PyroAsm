88b1ca9221494d13a7ea92ece303f1cd88309be6
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-06-23 02:49:31 +0200

    Feat : Ajout d'un compilateur de la fonction pyrint

--- bin/main.py
+++ bin/main.py
@@ -1,10 +1,49 @@
 import sys
+import re
+from colorama import init, Fore
 
-# Récupérer les arguments passés au script
+# Initialiser colorama
+init()
+
+class Compilateur:
+    def __init__(self):
+        # Liste des noms de fonctions autorisées
+        self.fonctions_autorisees = ["pyrint"]
+
+    def compiler(self, arguments):
+        # Afficher le message de début de compilation
+        print(Fore.GREEN + "La compilation a commencé." + Fore.RESET)
+
+        # Traiter les arguments
+        compteur_erreurs = 0  # Compteur d'erreurs
+        for argument in arguments:
+            with open(argument, "r") as file:
+                code = file.read()
+                matches = re.findall(r'([a-zA-Z_][a-zA-Z0-9_]*)\((.+?)\)', code)
+                for match in matches:
+                    fonction, value = match
+                    if fonction in self.fonctions_autorisees:
+                        value = value.strip('"')
+                        print(value)
+                    else:
+                        suggestion = None
+                        for f in self.fonctions_autorisees:
+                            if f.lower() == fonction.lower():
+                                suggestion = f
+                                break
+                        print(Fore.RED + "Erreur de frappe :", fonction)
+                        if suggestion:
+                            print("Vouliez-vous dire :", suggestion + "()" + Fore.RESET)
+                        compteur_erreurs += 1  # Incrémenter le compteur d'erreurs
+
+        # Afficher le message de fin de compilation et le nombre d'erreurs
+        print(Fore.GREEN + "La compilation est terminée." + Fore.RESET)
+        if compteur_erreurs == 0:
+            print(Fore.GREEN + "Aucune erreur de frappe détectée." + Fore.RESET)
+        else:
+            print(Fore.RED + "Nombre total d'erreurs de frappe :", str(compteur_erreurs) + Fore.RESET)
+
+# Utilisation de la classe Compilateur
 arguments = sys.argv[1:]  # Ignorer le premier argument (nom du script)
-
-# Traiter les arguments
-for argument in arguments:
-    print("Argument:", argument)
-    with open(argument, "r") as file:
-        print(file.read())
+compilateur = Compilateur()
+compilateur.compiler(arguments)
--- bin/pyroasm.bat
+++ bin/pyroasm.bat
@@ -1,5 +1,30 @@
 @echo off
 setlocal
+chcp 65001 > nul
 set "scriptPath=C:\Program Files\PyroAsm\bin\main.py"
 
-python "%scriptPath%" %*
+rem Vérifier si l'option "--version" est présente
+if "%~1"=="--version" (
+    echo Version 1.0
+    exit /b
+)
+
+rem Vérifier si l'option "--help" est présente
+if "%~1"=="--help" (
+    echo Liste des options:
+    echo --version: Affiche la version du compilateur
+    echo --help: Affiche la liste des options
+    echo -r: Compile le fichier spécifié
+)
+
+rem Vérifier si l'option "-r" est présente avec un fichier spécifié
+if "%~1"=="-r" (
+    if not "%~2"=="" (
+        rem Exécuter la commande pour compiler le fichier spécifié
+        python "%scriptPath%" "%~2"
+        exit /b
+    )
+)
+
+rem Si aucune option n'est fournie, exécuter le script Python
+echo Compilateur PyroASM 1.0
