3dfc481e6c1fe07056457dd4929798e99f46b3ee
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-06-26 01:04:51 +0200

    Feat : Ajout d'un systéme de fonction personaliser ( via fp )

--- bin/main.py
+++ bin/main.py
@@ -19,74 +19,23 @@ class Color(QWidget):
 class Compilateur:
     def __init__(self):
         # Liste des noms de fonctions autorisées
-        self.fonctions_autorisees = ["pyrint", "+", "-", "*", "/", "%", "^=", "const", "pyrowin", "fp"]
+        self.fonctions_autorisees = ["pyrint", "+", "-", "*", "/", "%", "^=", "const", "pyrowin", "fp", "NomDemAFunctonPersion"]
         self.fonctions_declarees = {}
 
     def compiler(self, arguments):
         # Afficher le message de début de compilation
         print(Fore.GREEN + "La compilation a commencé." + Fore.RESET)
-
         # Traiter les arguments
         compteur_erreurs = 0  # Compteur d'erreurs
         code_complet = ""
         for argument in arguments:
             with open(argument, "r", encoding="utf-8") as file:
-                self.executer_fonction("NomDemAFunctonPersion")
                 code = file.read()
-                # mettre tous sur une seul ligne
                 code = code.replace("\n", "")
                 code_complet += code
-                matches = re.findall(r'const\s+(\w+)\s*=\s*(.+)', code)
-                variables = {}
-
-                for match in matches:
-                    variable_name, value = match
-                    value = self.evaluate_expression(value, variables)
-                    variables[variable_name] = value
-
-                matches = re.findall(r'fp\s+(\w+)\s*\{([^}]+)\};', code_complet)  # Recherche des déclarations de fonctions
-                for match in matches:
-                    fonction_name, fonction_code = match
-                    self.fonctions_declarees[fonction_name] = fonction_code
-                    print("Déclaration de la fonction", fonction_name, ":", fonction_code)
-
-                matches = re.findall(r'([a-zA-Z_][a-zA-Z0-9_]*)\((.+?)\)', code)
-                for match in matches:
-                    fonction, value = match
-                    if fonction in self.fonctions_autorisees:
-                        if fonction not in self.fonctions_autorisees:
-                            continue
-                        if fonction == "pyrint":
-                            if value.startswith('"') and value.endswith('"'):
-                                value = value[1:-1]
-                            value = self.replace_variables(value, variables)  # Remplacer les variables
-                            print(value)
-                        if fonction == "pyrowin":
-                            dark_mode = True  
-                            if 'dark: false' in value:
-                                dark_mode = False
-
-                            if 'icon: ' in value or 'title: ' in value:
-                                pairs = value.split(',')
-                                icon_value = None
-                                title_value = None
-                                for pair in pairs:
-                                    if 'icon: ' in pair:
-                                        icon_value = pair.split('icon: ')[1].strip().strip('"')
-                                    elif 'title: ' in pair:
-                                        title_value = pair.split('title: ')[1].strip().strip('"')
-                                self.create_window(icon_value, title_value, dark_mode)
-                    else:
-                        suggestion = None
-                        for f in self.fonctions_autorisees:
-                            if f.lower() == fonction.lower():
-                                suggestion = f
-                                break
-                        print(Fore.RED + "Erreur de frappe :", fonction)
-                        if suggestion:
-                            print("Vouliez-vous dire :", suggestion + "()" + Fore.RESET)
-                        compteur_erreurs += 1
-
+                self.process_function_declarations(code_complet)
+        self.process_function_calls(code_complet)
+                
         # Afficher le message de fin de compilation et le nombre d'erreurs
         print(Fore.GREEN + "La compilation est terminée." + Fore.RESET)
         if compteur_erreurs == 0:
@@ -94,15 +43,59 @@ class Compilateur:
         else:
             print(Fore.RED + "Nombre total d'erreurs de frappe :", str(compteur_erreurs) + Fore.RESET)
 
-    def executer_fonction(self, fonction_name):
-        print("Exécution de la fonction", fonction_name)
-        if fonction_name in self.fonctions_declarees:
-            fonction_code = self.fonctions_declarees[fonction_name]
-            print("Code de la fonction", fonction_name, ":", fonction_code)
-            self.compiler([fonction_code + ";"])  # Compiler le code de la fonction
-        else:
-            print(Fore.RED + "Erreur : La fonction", fonction_name, "n'est pas déclarée." + Fore.RESET)
 
+    def process_const_declarations(self, code):
+        matches = re.findall(r'const\s+(\w+)\s*=\s*(.+)', code)
+        variables = {}
+
+        for match in matches:
+            variable_name, value = match
+            value = self.evaluate_expression(value, variables)
+            variables[variable_name] = value
+
+        return variables
+    
+
+    def process_function_declarations(self, code):
+        matches = re.findall(r'fp\s+(\w+)\s*\(\)\s*\{([^}]+)\}', code)
+        for match in matches:
+            fonction_name, fonction_code = match
+            self.fonctions_declarees[fonction_name] = fonction_code
+        
+
+    def process_function_calls(self, code):
+        pattern = r'(\w+)\(\)'
+        matches = re.findall(pattern, code)
+        for match in matches:
+            function_name = match
+            self.executer_fonction(function_name)
+
+
+    def executer_fonction(self, fonction_name):
+        
+        # regarder dans liste
+        if fonction_name in self.fonctions_declarees:
+            # récupe le code de la fonction
+            fonction_code = self.fonctions_declarees[fonction_name]
+            
+            # exécuter le code de la fonction
+            self.executer_code(code=fonction_code)
+            
+        
+    # Créé une def pour excecuter le code
+    def executer_code(self, code):
+        matches = re.findall(r'pyrint\s*\(\s*(.+)\s*\)', code)
+        for match in matches:
+            value = match.replace('"', '')
+            print(value)
+        
+        matches = re.findall(r'pyrowin\s*\(\s*(.+)\s*\)', code)
+        for match in matches:
+            value = match.replace('"', '')
+            print(value)
+            self.create_window(Icon=None, Title=value, DarkMode=True)
+             
+            
     def replace_variables(self, value, variables):
         def replace_variable(match):
             variable_name = match.group(1)
@@ -114,6 +107,8 @@ class Compilateur:
         value = re.sub(r'\$(\w+)', replace_variable, value)
         return value
     
+
+
     def create_window(self, Icon, Title, DarkMode):
         if DarkMode == True:
             DarkMode = (12, 12, 13)
